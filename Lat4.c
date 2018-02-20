940515d79607a56b9cda016c1ee7a4316dc9f881
mnaufalfap <36650560+mnaufalfap@users.noreply.github.com>
2018-02-20 08:00:03 +0700

    Latihan 4

---
+++ Lat4.c
@@ -0,0 +1,18 @@
+/*
+Program			: Hello4.c
+Deskripsi		: Menampilkan Hello. Deklarasi & Initialisasi
+Author			:
+Versi / tanggal	: 1/13-10-2017
+Comiler			: Dev-C++
+
+=================================================================== */
+#include <stdio.h>
+
+int main ()
+{ /* deklarasi */
+	char nama[15]="Gusdur ...";
+	
+	/* BODY / Tubuh Program */
+	printf("Hello .. \n%s", nama);
+	return 0;	
+}
